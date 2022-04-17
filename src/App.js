1c02cbd270be6e6c2af3dcaee0c389eabf661891
AbdMamun1999 <mamunrox@gmail.com>
2022-04-18 04:18:13 +0600

    Protected CheckOut page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CheckOut from './Pages/CheckOut/CheckOut';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -14,7 +15,11 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/services/:serviceId' element={<CheckOut></CheckOut>}></Route>
+        <Route path='/services/:serviceId' element={
+          <RequireAuth>
+            <CheckOut></CheckOut>
+          </RequireAuth>
+        }></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loging';
 
@@ -9,6 +9,9 @@ const Login = () => {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
     const navigate = useNavigate()
+    const location = useLocation()
+
+    let from = location.state?.from?.pathname || '/'
 
     const [
         signInWithEmailAndPassword,
@@ -30,13 +33,12 @@ const Login = () => {
       }
 
       if(user){
-          navigate('/')
+          navigate(from,{replace:true});
       }
 
       const handleLogInSubmit = event =>{
           event.preventDefault()
           signInWithEmailAndPassword(email,password)
-        //   navigate('/')
 
       }
 
