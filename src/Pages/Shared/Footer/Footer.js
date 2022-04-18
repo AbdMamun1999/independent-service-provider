a0452c6bc5624e30dd79fdf414f2b25da7685c3c
AbdMamun1999 <mamunrox@gmail.com>
2022-04-18 21:16:25 +0600

    add footer component

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 const Footer = () => {
+    const date = new Date().getFullYear()
     return (
-        <div>
-            <h2>This is Footer</h2>
+        <div className='bg-dark mt-5 text-white p-5'>
+            <div className=' d-flex justify-content-between align-items-center'>
+                <p className='text-white'><small>copy right &copy {date}</small></p>
+                <div>
+                    <h4>
+                        CONTACT
+                    </h4>
+                    <div>
+                        <p>
+                            <span>Phone: 0170000000</span>
+                        </p>
+                        <p>
+                        <span>Email: abc@gmail.com</span>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h4>ADDRESS</h4>
+                    <address>Maniknagar,Mugda,Dhaka</address>
+                </div>
+            </div>
         </div>
     );
 };
